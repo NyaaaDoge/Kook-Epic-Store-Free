865abd4c58c95f3d244fdef38a0f325ecbf61605
NyaaaDoge <NyaaaDoge@gmail.com>
2022-11-02 22:48:14 +0800

    refactor: loggings

--- botutils/epic_store_core.py
+++ botutils/epic_store_core.py
@@ -1,6 +1,8 @@
 import logging
 import aiohttp
 
+logger = logging.getLogger(__name__)
+
 EPIC_FREE_GAMES_URL = "https://store-site-backend-static-ipv4.ak.epicgames.com/freeGamesPromotions?locale=zh-CN&country=CN&allowCountries=CN"
 
 
@@ -10,18 +12,17 @@ async def getEpicFreeGames() -> list:
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
     }
     try:
-        logging.info(f"Getting epic store free games from API...")
+        logger.info(f"Getting epic store free games from API...")
         async with aiohttp.ClientSession() as session:
             async with session.get(EPIC_FREE_GAMES_URL, headers=headers) as response:
                 response_json = await response.json()
                 if any(response_json):
                     free_games = response_json["data"]["Catalog"]["searchStore"]["elements"]
-                    logging.info(f"Got {len(free_games)} item(s) from API.")
+                    logger.info(f"Got {len(free_games)} item(s) from API.")
                     return free_games
                 else:
-                    logging.error(f"No Epic Free Games")
+                    logger.error(f"No Epic Free Games")
                     return []
     except Exception as e:
-        logging.exception(f"Get Epic Free Games Exception: {e}")
+        logger.exception(f"Get Epic Free Games Exception: {e}")
         return []
-
